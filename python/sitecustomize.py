dd8c90d5676054e3d0951347a7d78a2c757cff1a
Jari Rosti <jrosti@nvidia.com>
2019-04-08 10:34:18 +0300

    Iterate over copy of sys.path in sitecustomize
    Integration to VS Code ptvsd debugger fails, because path
    is changed as a side effect of site.addsitedir.

--- python/sitecustomize.py
+++ python/sitecustomize.py
@@ -4,6 +4,6 @@ import site
 # Call site.addsitedir() for all PYTHONPATH entries inside runfiles.
 # This causes any .pth files to be processed.  Without this, .pth files are not processed
 # and some namespace packages (like google.*) will not work.
-for p in sys.path:
+for p in list(sys.path):
     if ".runfiles/" in p:
         site.addsitedir(p)
